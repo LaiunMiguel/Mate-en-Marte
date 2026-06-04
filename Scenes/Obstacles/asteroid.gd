f0be54e0a73491b654068ea61e016cc0b66a3c54
Laiun Miguel <LaiunMiguel@hotmail.com>
2026-06-03 22:57:34 -0300

    agregado sprite temp para el segundo obstaculo y removido su habilidad de caer

--- Scenes/Obstacles/asteroid.gd
+++ Scenes/Obstacles/asteroid.gd
@@ -12,10 +12,6 @@ func initialize(position:Vector2, size:float , speed: float):
 func _on_visible_on_screen_notifier_2d_screen_exited() -> void:
 	queue_free()
 
-func _process(delta: float) -> void:
-	position.y += obstacle_speed * delta 
-
-	
 func _on_body_entered(body: Node2D) -> void:
 	if body.is_in_group("player"):
 		body.get_hit(self)
